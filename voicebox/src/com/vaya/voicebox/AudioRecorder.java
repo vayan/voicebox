1746324513c57b265628a0857cae3a90a8a33f49
Yann Vaillant <yann@vaillant.im>
2014-01-01 20:11:00 +0800

    record audio in service

---
+++ voicebox/src/com/vaya/voicebox/AudioRecorder.java
@@ -0,0 +1,89 @@
+package com.vaya.voicebox;
+
+import java.io.IOException;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+public class AudioRecorder extends IntentService {
+	
+	private MediaRecorder mRecord = null;
+	private String Filename = null;
+	
+	public AudioRecorder() {
+		super("AudioRecorderServiceu");
+	}
+	
+	public void SetFilename(String name) {
+		Filename = Environment.getExternalStorageDirectory().getAbsolutePath();
+		Filename += "/"+name+".3gp";
+	}
+
+
+	private void StartRecord() {
+		SetFilename("testtesttest");
+		mRecord = new MediaRecorder();
+		mRecord.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecord.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecord.setOutputFile(Filename);
+        mRecord.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        
+        
+        try {
+            mRecord.prepare();
+			Log.d(MainActivity.LOG_TAG, "StartRecord() go");
+
+        } catch (IOException e) {
+            Log.e(MainActivity.LOG_TAG, "prepare() failed : " + e.getMessage());
+        }
+
+        mRecord.start();
+	}
+	
+	private void PauseRecord(){
+		
+	}
+	
+	private void StopRecord() {
+		if (mRecord == null) {
+			Log.e(MainActivity.LOG_TAG, "StopRecord() failed, no recording");
+			return;
+		}
+		mRecord.stop();
+        mRecord.release();
+        mRecord = null;
+        Log.d(MainActivity.LOG_TAG, "StopRecord() go");
+	}
+	
+
+
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		 Log.d(MainActivity.LOG_TAG, "Service binded");
+		 
+		return null;
+	}
+
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		StartRecord();
+		long endTime = System.currentTimeMillis() + 5*1000;
+	      while (System.currentTimeMillis() < endTime) {
+	          synchronized (this) {
+	              try {
+	                  wait(endTime - System.currentTimeMillis());
+	              } catch (Exception e) {
+	              }
+	          }
+	      }
+	      StopRecord();
+	}
+
+}
--- voicebox/src/com/vaya/voicebox/MainActivity.java
+++ voicebox/src/com/vaya/voicebox/MainActivity.java
@@ -2,9 +2,20 @@ package com.vaya.voicebox;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	public static final String LOG_TAG = "VoiceBox";
+	
+	public void ToggleRecord(View view) {
+		 Log.d(MainActivity.LOG_TAG, "ToggleRecord() hit");
+		 Intent intent = new Intent(this, AudioRecorder.class);
+		 startService(intent);
+	 }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
