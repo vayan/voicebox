7688d25a348ee2248ecf5b4b14824e68c8027718
Yann Vaillant <yann@vaillant.im>
2014-01-04 01:12:30 +0800

    settings added

--- voicebox/src/com/vaya/voicebox/SettingsActivity.java
+++ voicebox/src/com/vaya/voicebox/SettingsActivity.java
@@ -2,14 +2,28 @@ package com.vaya.voicebox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends Activity {
+	
+	public static class SettingsFragment extends PreferenceFragment {
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+
+	        // Load the preferences from an XML resource
+	        addPreferencesFromResource(R.xml.preferences);
+	    }
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_settings);
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
 	}
 
 }
