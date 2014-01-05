34cff5f16a4667f8b9069b0a867528d4a6dc89c5
Satyre <robin.maitre@gmail.com>
2014-01-05 17:42:52 +0800

    number picker

--- voicebox/src/com/vaya/voicebox/SettingsActivity.java
+++ voicebox/src/com/vaya/voicebox/SettingsActivity.java
@@ -2,17 +2,24 @@ package com.vaya.voicebox;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
+import android.widget.TextView;
 
 public class SettingsActivity extends Activity  {
 
 	public static final String LOG_TAG = "VoiceBoxSettings";
-
+	 private TextView tv;
 	public void updateTheme() {
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -69,4 +76,44 @@ public class SettingsActivity extends Activity  {
 		.replace(android.R.id.content, new SettingsFragment())
 		.commit();
 	}
+	
+	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+        Log.i("value is",""+newVal);
+
+    }
+	
+	 public void show()
+	    {
+		  
+	         final Dialog d = new Dialog(SettingsActivity.this);
+	         d.setTitle("NumberPicker");
+	         d.setContentView(R.xml.dialog);
+	         Button b1 = (Button) d.findViewById(R.id.button1);
+	         Button b2 = (Button) d.findViewById(R.id.button2);
+	         final NumberPicker np = (NumberPicker) d.findViewById(R.id.numberPicker1);
+	         np.setMaxValue(100); // max value 100
+	         np.setMinValue(0);   // min value 0
+	         np.setWrapSelectorWheel(false);
+	         np.setOnValueChangedListener((OnValueChangeListener) this);
+	         b1.setOnClickListener(new OnClickListener()
+	         {
+	          @Override
+	          public void onClick(View v) {
+	              tv.setText(String.valueOf(np.getValue())); //set the value to textview
+	              d.dismiss();
+	           }    
+	          });
+	         b2.setOnClickListener(new OnClickListener()
+	         {
+	          @Override
+	          public void onClick(View v) {
+	              d.dismiss(); // dismiss the dialog
+	           }    
+	          });
+	       d.show();
+
+
+	    }
 }
+
