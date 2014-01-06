a12bb00d6f4de8434b07d72952f4fdbb3c2f503a
Satyre <robin.maitre@gmail.com>
2014-01-07 01:29:04 +0800

    bug number picker.....

--- voicebox/src/com/vaya/voicebox/NumberPickerPreference.java
+++ voicebox/src/com/vaya/voicebox/NumberPickerPreference.java
@@ -4,10 +4,23 @@ import android.app.Dialog;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnValueChangeListener;
 
 public class NumberPickerPreference extends DialogPreference {
+	/*@Override
+	protected void onBindDialogView(View view) {
+		
+		NumberPicker np = (NumberPicker) view.findViewById(R.id.numberPicker1);
+		  np.setMaxValue(100); // max value 100
+	         np.setMinValue(0);   // min value 0
+	         np.setWrapSelectorWheel(false);
+	         np.setOnValueChangedListener((OnValueChangeListener) this);
+	        
+	super.onBindDialogView(view);
+	}*/
+	
 	    public NumberPickerPreference(Context context, AttributeSet attrs) {
 	        super(context, attrs);
 	     
@@ -15,12 +28,7 @@ public class NumberPickerPreference extends DialogPreference {
 	        setPositiveButtonText(android.R.string.ok);
 	        setNegativeButtonText(android.R.string.cancel);
 
-	        NumberPicker np = (NumberPicker) Context.getApplicationContext().findViewById(R.id.numberPicker1);
-	         np.setMaxValue(100); // max value 100
-	         np.setMinValue(0);   // min value 0
-	         np.setWrapSelectorWheel(false);
-	         np.setOnValueChangedListener((OnValueChangeListener) this);
-	        
+	       
 	        setDialogIcon(null);
 	        
 	    }
